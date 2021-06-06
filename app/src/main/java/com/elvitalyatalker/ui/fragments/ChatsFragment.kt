8c6ad171341a5c05b7dd5e4365d66cb484843b3f
Vitaly <kuraev_vf@hotmail.com>
2021-06-06 19:31:25 +0500

    code refactoring

---
+++ app/src/main/java/com/elvitalyatalker/ui/fragments/ChatsFragment.kt
@@ -0,0 +1,14 @@
+package com.elvitalyatalker.ui.fragments
+
+import com.elvitalyatalker.R
+import com.elvitalyatalker.databinding.FragmentChatsBinding
+
+
+class ChatsFragment : BaseFragment(R.layout.fragment_chats) {
+    private lateinit var mBinding: FragmentChatsBinding
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+}
---
+++ app/src/main/java/com/elvitalyatalker/ui/fragments/SettingsFragment.kt
@@ -0,0 +1,14 @@
+package com.elvitalyatalker.ui.fragments
+
+import com.elvitalyatalker.R
+import com.elvitalyatalker.databinding.FragmentSettingsBinding
+
+
+class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
+    private lateinit var mBinding: FragmentSettingsBinding
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+}
