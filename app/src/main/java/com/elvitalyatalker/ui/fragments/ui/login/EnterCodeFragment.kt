df2075b88dc0fe7a6f7349cefac52f37e290f4b7
Vitaly <kuraev_vf@hotmail.com>
2021-06-07 14:30:23 +0500

    enter code and enter phone fragmetns added

--- app/src/main/java/com/elvitalyatalker/ui/fragments/ui/login/EnterCodeFragment.kt
+++
@@ -1,9 +0,0 @@
-package com.elvitalyatalker.ui.fragments.ui.login
-
-import androidx.fragment.app.Fragment
-import com.elvitalyatalker.R
-
-
-class EnterCodeFragment : Fragment(R.layout.fragment_enter_code) {
-
-}
