54a55bcf532478d3409883dba8eaa0c8539f9b13
Vitaly <kuraev_vf@hotmail.com>
2021-06-07 10:46:10 +0500

    setting option menu added

--- app/src/main/java/com/elvitalyatalker/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/elvitalyatalker/ui/fragments/SettingsFragment.kt
@@ -1,5 +1,7 @@
 package com.elvitalyatalker.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import com.elvitalyatalker.R
 import com.elvitalyatalker.databinding.FragmentSettingsBinding
 
@@ -9,6 +11,11 @@ class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu, menu)
     }
 }
