07d345d10eeb646d4b2620507b64347f66e00dfc
Vitaly <kuraev_vf@hotmail.com>
2021-06-08 15:11:20 +0500

    Refactoring, added BIO change options

--- app/src/main/java/com/elvitalyatalker/ui/fragments/ChangeUsernameFragment.kt
+++ app/src/main/java/com/elvitalyatalker/ui/fragments/ChangeUsernameFragment.kt
@@ -13,28 +13,16 @@ import java.util.*
 import kotlin.concurrent.fixedRateTimer
 
 
-class ChangeUsernameFragment : BaseFragment(R.layout.fragment_change_username) {
+class ChangeUsernameFragment : BaseChangeFragment(R.layout.fragment_change_username) {
 
     lateinit var mNewUsername: String
 
     override fun onResume() {
         super.onResume()
-        setHasOptionsMenu(true)
         settings_input_username.setText(USER.username)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        (activity as MainActivity).menuInflater.inflate(R.menu.settings_menu_confirm, menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.settings_confirm_change -> change()
-        }
-        return true
-    }
-
-    private fun change() {
+    override fun change() {
         mNewUsername = settings_input_username.text.toString().lowercase()
         if (mNewUsername.isEmpty()) {
             showToast("Поле пустое")
